9b200d597b4444a231f7df3e2b3d2b3f6e525f9e
1halfplusminus <dartuchiwa@gmail.com>
2021-05-08 11:00:41 +1100

    Anagrams

--- examples/transform/anagrams.ts
+++ examples/transform/anagrams.ts
@@ -4,13 +4,23 @@ import {
   mapValues,
   toArray,
   uniq,
-  union,
-  uniqBy,
-  isEqual,
+  indexBy,
+  join,
 } from "lodash/fp";
 import pipe from "lodash/fp/pipe";
 
-const codepoints = (string: String) => {
+type Dictionary = { [key: string]: string };
+
+function generate(words: string[]): Dictionary {
+  return pipe(indexBy(pipe(codepoints, sortAlphabets)))(words);
+}
+function look_up_by_signature(dict: Dictionary) {
+  return (key: string) => {
+    return dict[key];
+  };
+}
+
+const codepoints = (string: string) => {
   return [...string];
 };
 const subsets = <T>(theArray: T[]) =>
@@ -20,13 +30,13 @@ const subsets = <T>(theArray: T[]) =>
     [[]]
   );
 const longerThan = <T>(length: number) =>
-  pipe(filter((v: T[]) => v.length > length));
+  pipe(filter((v: T[]) => v.length >= length));
 
 const groupByLength = <T>(array: T[][]) => groupBy("length", array);
 
-const sortAlphabets = function (charsOrString: String[] | String) {
+const sortAlphabets = function (charsOrString: string[] | string) {
   const chars =
-    charsOrString instanceof String ? charsOrString.split("") : charsOrString;
+    typeof charsOrString == "string" ? charsOrString.split("") : charsOrString;
   return chars.sort().join("");
 };
 
@@ -35,6 +45,29 @@ const asSignature = pipe(mapValues(sortAlphabets), toArray, uniq);
 export const all_subset_longer_than = (length: number) =>
   pipe(codepoints, subsets, longerThan(length));
 
-console.log(
-  pipe(all_subset_longer_than(3), asSignature, groupByLength)("vinyl")
-);
+export const find_in_directory = (words: string[]) => (signatures: string[]) =>
+  pipe(
+    mapValues(pipe(look_up_by_signature(generate(words)))),
+    mapValues(join("")),
+    filter((v) => !!v)
+  )(signatures);
+
+export const anagrams_in = (word: string) =>
+  pipe(
+    all_subset_longer_than(3),
+    asSignature,
+    find_in_directory([
+      "vinyl",
+      "test",
+      "pizza",
+      "ivy",
+      "nil",
+      "yin",
+      "inly",
+      "liny",
+      "viny",
+    ]),
+    groupByLength
+  )(word);
+
+console.log(anagrams_in("vinyl"));
