ae3143400b99f852994930904ae4b65e574b395e
1halfplusminus <dartuchiwa@gmail.com>
2021-05-09 09:55:38 +1100

    09_05

--- examples/transform/anagrams.ts
+++ examples/transform/anagrams.ts
@@ -7,9 +7,55 @@ import {
   indexBy,
   join,
   pipe,
+  truncate,
 } from "lodash/fp";
+import { readFileSync } from "fs";
 
 type Dictionary = { [key: string]: string };
+type ResultOk<T> = ["ok", T];
+
+type ResultError<T> = ["error", T];
+module File {
+  export function open(path: string) {
+    try {
+      const result = readFileSync("./test");
+      return ["ok", result] as ResultOk<Buffer>;
+    } catch (e) {
+      return ["error", e.message] as ResultError<string>;
+    }
+  }
+}
+
+function find_matching_lines(pattern: string) {
+  return (content: ResultOk<Buffer> | ResultError<any>) => {
+    if (content[0] == "ok") {
+      return pipe(
+        (r: Buffer) => {
+          return r.toString().split("/n");
+        },
+        filter((s: string) => s.includes(pattern))
+      )(content[1]);
+    } else {
+      return ["error", content[1]];
+    }
+  };
+}
+
+function truncate_lines(content: ResultOk<string[]> | ResultError<any>) {
+  if (content[0] == "ok") {
+    return pipe(mapValues(truncate({ length: 20 })), toArray)(content[1]);
+  } else {
+    return ["error", content[1]];
+  }
+}
+
+function find_all(fileName: string, pattern: string) {
+  return pipe(
+    File.open,
+    find_matching_lines(pattern),
+    truncate_lines
+  )(fileName);
+}
 
 function generate(words: string[]): Dictionary {
   return pipe(indexBy(pipe(codepoints, sortAlphabets)))(words);
