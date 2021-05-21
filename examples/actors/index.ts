ea89ce45ad494af73160d44d9d464145704a2fb3
1halfplusminus <dartuchiwa@gmail.com>
2021-05-21 11:32:22 +1100

    Actor & Processes

--- examples/actors/index.ts
+++ examples/actors/index.ts
@@ -1,1 +1,30 @@
-import {} from "nact";
+import { ActorFunc, start, dispatch, stop, spawnStateless, spawn } from "nact";
+
+declare interface State {
+  waiter: string;
+}
+declare type HungryForPie = {
+  type: "hungry for pie";
+};
+declare type Order = {
+  type: "order";
+  customer: string;
+  wants: string;
+};
+declare type Message = Order | HungryForPie;
+
+const system = start();
+
+const customerActor = spawn(
+  system,
+  (state: State, msg: Message, ctx) => {
+    if (msg.type === "hungry for pie") {
+      return dispatch(state.waiter, {
+        type: "order",
+        customer: ctx.self,
+        wants: "pie",
+      });
+    }
+  },
+  "customer"
+);
